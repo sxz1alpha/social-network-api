4bbdeff7ff7b53fa3ce717040bfe3be7783d2327
UCDT <adrian.j.ulibarri@gmail.com>
2021-08-20 19:59:44 -0600

    Finished user model.

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema ({
+        userName: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please use a valid email address.']
+        },
+        thoughts: [{
+            type: Schema.Type.ObjectId,
+            ref: 'thought'
+        }],
+        friends: [{
+            type: Schema.Type.ObjectId,
+            ref: 'friends'
+        }]
+    },
+    {
+        toJSON: {
+            vituals: true,
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
